01434b20105dc4b339097bef3be3b2862cb37b7b
kksua <kanampalla.amarajeeva@groupe-esigelec.org>
2024-03-12 14:21:32 +0100

    Creer la classe Chanteur

---
+++ SiteWeb_ISN/Chanteur.cs
@@ -0,0 +1,10 @@
+﻿namespace SiteWeb_ISN
+{
+    public class Chanteur
+    {
+        public int IdChanteur { get; set; }
+        public string Nom { get; set; } = string.Empty;
+        public string nationaliteChanteur { get; set; } = string.Empty;
+        public byte[] Image { get; set; }
+    }
+}
