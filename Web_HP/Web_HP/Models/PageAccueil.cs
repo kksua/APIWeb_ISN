57628feadd9cb02e8ddf28cf777b85235b8b5972
kksua <kanampalla.amarajeeva@groupe-esigelec.org>
2024-03-26 13:29:49 +0100

    Ajouter le nav bar

---
+++ Web_HP/Web_HP/Models/PageAccueil.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_HP.Models
+{
+    public class PageAccueil
+    {
+        public string WelcomeMessage { get; set; }
+        public PageAccueil()
+        {
+            // Set default values for properties
+            WelcomeMessage = "Bienvenue!";
+        }
+    }
+}
