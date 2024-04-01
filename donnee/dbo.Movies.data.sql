740b35012173b5ad62db27c7ed8e6fb540db4039
Boya FU <boya.fu@groupe-esigelec.org>
2024-04-01 18:28:23 +0200

    nouvelle

---
+++ donnee/dbo.Movies.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Movies (idFilm,NomFilm, Duree, Image, Description)
+VALUES (1,'Interstellar', '2h 49m', 'https://images.app.goo.gl/XyMbtd5bR8gnvMJ97', 'Set in a dystopian future where humanity is embroiled in a catastrophic blight and famine, the film follows a group of astronauts who travel through a wormhole near Saturn in search of a new home for humankind.');
