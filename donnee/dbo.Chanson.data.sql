8b9b0068a0a62f7fa0c1d33340a5ab04a4ca4617
Boya FU <boya.fu@groupe-esigelec.org>
2024-04-01 18:01:17 +0200

    donnees

---
+++ donnee/dbo.Chanson.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Chanson ( NomChanson, Genre, Langue, Image, Lien)
+VALUES 
+    ('Blanche nuit', 'jazz', 'chinois', 'https://wx4.sinaimg.cn/mw690/008lgPsGgy1hm7ywqg5wij31hc140nca.jpg', 'https://youtu.be/KOa-FlZ3EyI?si=5VqUKZnJYnXzGXr1'),
+    (' God of Music', 'happy', 'coréen', 'https://i.scdn.co/image/ab67616d0000b273d07a54abba4f5060c2486e3c', 'https://youtu.be/zSQ48zyWZrY?si=n-VvhGBwkcSTFBAc');
