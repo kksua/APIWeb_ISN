6e0dce2a32504f855ed2cdc523852821c594ba3a
Supipi Amarajeeva <148068360+kksua@users.noreply.github.com>
2024-04-02 11:03:03 +0200

    Update dbo.Album.data.sql

--- donnee/dbo.Album.data.sql
+++ donnee/dbo.Album.data.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO Album (NomAlbum, Image, ChanteurIdChanteur)
+INSERT INTO Album (NomAlbum, Image, ChanteurIdChanteur)
 VALUES 
     ('WHITE NIGHT', 'https://i0.hdslb.com/bfs/archive/6d4241ab01bd1323040f694b17fa8bb5358bb0eb.jpg', NULL),
-    ('SEVENTEEN 11th Mini Album ''SEVENTEENTH HEAVEN''', 'https://i.scdn.co/image/ab67616d0000b273d07a54abba4f5060c2486e3c', NULL);
+    ('SEVENTEEN 11th Mini Album - SEVENTEENTH HEAVEN', 'https://i.scdn.co/image/ab67616d0000b273d07a54abba4f5060c2486e3c', NULL);
