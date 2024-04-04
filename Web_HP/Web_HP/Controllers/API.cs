38a6d2f5c86421c155422729c55a2c400c0dc8c6
kksua <kanampalla.amarajeeva@groupe-esigelec.org>
2024-04-04 10:11:15 +0200

    code finale avec bonne connexion

--- Web_HP/Web_HP/Controllers/API.cs
+++ Web_HP/Web_HP/Controllers/API.cs
@@ -11,18 +11,22 @@ namespace Web_HP.Controllers
 {
     public sealed class API
     {
+        // HttpClient pour effectuer les requêtes HTTP
         private static readonly HttpClient client = new HttpClient();
 
         private API()
         {
+            // Configuration de l'adresse de base de l'API
             client.BaseAddress = new Uri("http://localhost:5084");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
         private static readonly object padlock = new object();
+        // Instance unique de la classe API
         private static API? instance = null;
 
+        // Propriété publique statique pour accéder à l'instance unique de la classe
         public static API Instance
         {
             get
@@ -38,6 +42,7 @@ namespace Web_HP.Controllers
             }
         }
 
+        // Méthode pour récupérer un utilisateur par son ID
         public async Task<Utilisateur> GetUtilisateur(int id)
         {
             Utilisateur? utilisateur = null;
@@ -52,6 +57,7 @@ namespace Web_HP.Controllers
 #pragma warning restore CS8603 // Existence possible d'un retour de référence null.
         }
 
+        // Méthode pour récupérer un utilisateur par son ID en tant que chaîne de caractères
         public async Task<Utilisateur?> GetUtilisateur(string idString)
         {
             int id;
@@ -59,7 +65,7 @@ namespace Web_HP.Controllers
                 return await GetUtilisateur(id);
             return null;
         }
-
+        // Méthode pour vérifier la connexion d'un utilisateur
         public async Task<Utilisateur> VerifyLogin(string email, string pwd)
         {
             Utilisateur? utilisateur = null;
