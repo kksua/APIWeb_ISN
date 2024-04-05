66ff8a374422bc9cb57ea09e86ebaa791a0d8a5f
Supipi Amarajeeva <148068360+kksua@users.noreply.github.com>
2024-04-05 15:32:40 +0200

    Update dbo.Chanson.data.sql

--- donnee/dbo.Chanson.data.sql
+++ donnee/dbo.Chanson.data.sql
@@ -2,3 +2,9 @@
 VALUES 
     ('Blanche nuit', 'jazz', 'chinois', 'https://wx4.sinaimg.cn/mw690/008lgPsGgy1hm7ywqg5wij31hc140nca.jpg', 'https://youtu.be/KOa-FlZ3EyI?si=5VqUKZnJYnXzGXr1'),
     ( 'God of Music', 'happy', 'coréen', 'https://i.scdn.co/image/ab67616d0000b273d07a54abba4f5060c2486e3c', 'https://youtu.be/zSQ48zyWZrY?si=n-VvhGBwkcSTFBAc');
+('Matilda' , 'Ballad' , 'anglais' , 'https://t2.genius.com/unsafe/504x504/https%3A%2F%2Fimages.genius.com%2Fd98059a0f2afd10b04b912355c0e8ceb.1000x1000x1.png', 'https://youtu.be/e6MpCYZlBMk');
+('What Was I Made For?', 'Indie' , 'https://upload.wikimedia.org/wikipedia/en/5/54/Billie_Eilish_-_What_Was_I_Made_For%3F.png' , 'https://youtu.be/cW8VLC9nnTo');
+('Unwritten', 'Rhythm and blues', 'https://i.scdn.co/image/ab67616d0000b27349e0dac1080e8838054dd025','https://youtu.be/b7k0a5hYnSI');
+
+
+
