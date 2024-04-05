2c7066574b6434aea44a1ce6bb310d1cd28742b2
Supipi Amarajeeva <148068360+kksua@users.noreply.github.com>
2024-04-05 14:41:11 +0200

    Update dbo.Series.data.sql

--- donnee/dbo.Series.data.sql
+++ donnee/dbo.Series.data.sql
@@ -1,2 +1,7 @@
 ﻿INSERT INTO Series (NomSerie, NmbreSeasons, NmbreEpisode)
 VALUES ('Nirvana in Fire', 2, 54);
+VALUES ('Friends', 10, 236);
+VALUES ('How i met your mother', 9, 208);
+VALUES ('The Big Bang Theory', 12 , 279);
+
+
