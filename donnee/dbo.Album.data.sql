e54d28aa87c19db51701396ce63c18bce652067e
Supipi Amarajeeva <148068360+kksua@users.noreply.github.com>
2024-04-05 15:36:48 +0200

    Update dbo.Album.data.sql

--- donnee/dbo.Album.data.sql
+++ donnee/dbo.Album.data.sql
@@ -2,3 +2,6 @@
 VALUES 
     ('WHITE NIGHT', 'https://i0.hdslb.com/bfs/archive/6d4241ab01bd1323040f694b17fa8bb5358bb0eb.jpg', NULL),
     ('SEVENTEEN 11th Mini Album ''SEVENTEENTH HEAVEN''', 'https://i.scdn.co/image/ab67616d0000b273d07a54abba4f5060c2486e3c', NULL);
+('1989 (Taylor's Version)', 'https://m.media-amazon.com/images/I/616Xa+hzMXL._UF1000,1000_QL80_.jpg', NULL);
+    ('Fearless (Taylor's Version)','https://media.newyorker.com/photos/60747954c920e996bd1e486d/1:1/w_1326,h_1326,c_limit/Battan-FearlessTaylorsVersion.jpg', NULL);
+('Midnights','https://upload.wikimedia.org/wikipedia/en/9/9f/Midnights_-_Taylor_Swift.png,NULL);
