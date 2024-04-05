f403afad5f8ac67670d343cb6481864d16fd99eb
Supipi Amarajeeva <148068360+kksua@users.noreply.github.com>
2024-04-05 14:35:00 +0200

    Update dbo.Movies.data.sql

--- donnee/dbo.Movies.data.sql
+++ donnee/dbo.Movies.data.sql
@@ -1,2 +1,6 @@
 ﻿INSERT INTO Movies (idFilm,NomFilm, Duree, Image, Description)
 VALUES (1,'Interstellar', '2h 49m', 'https://images.app.goo.gl/XyMbtd5bR8gnvMJ97', 'Set in a dystopian future where humanity is embroiled in a catastrophic blight and famine, the film follows a group of astronauts who travel through a wormhole near Saturn in search of a new home for humankind.');
+VALUES (2, 'The Boys in the Boat' , '2h 4m', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPu0jDbb5ltteOnDNLpfnBC6kFGR1Jj4D8YZp-GKlMHsI4mBcn' , 'Au plus fort de la Grande Dépression, les membres de l'équipe d'aviron de l'Université de Washington sont mis sous les projecteurs alors qu'ils concourent pour l'or aux Jeux olympiques de 1936 à Berlin.');
+VALUES (3, 'Top Gun: Maverick' , '2h 11m' , 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTb0I81g39NGDx0sKP5F_DECCsdzoOwh9MVUD0PLSVvQpf3rpIM' , 'Trente années de service amènent Maverick à former un groupe de diplômés d'élite de TOPGUN pour se préparer à une mission de grande envergure pendant que Maverick combat ses démons du passé.'
+
+
